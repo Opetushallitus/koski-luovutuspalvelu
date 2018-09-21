2f78a6aad2cfb83290f2cef3c5c64246061d2f6d
Pasi Eronen <pasi.eronen@reaktor.com>
2018-09-21 22:14:55 +0300

    Include Travis build number in buildversion.txt; print it when starting certbot

--- proxy/scripts/create-buildversion.sh
+++ proxy/scripts/create-buildversion.sh
@@ -7,6 +7,7 @@ version=${KOSKI_VERSION:-local}
 cat >$file <<EOL
 artifactId=koski-luovutuspalvelu-proxy
 version=$version
+buildNumber=${TRAVIS_BUILD_NUMBER:-unknown}
 vcsRevision=`git rev-parse HEAD`
 buildDate=`date`
 EOL
