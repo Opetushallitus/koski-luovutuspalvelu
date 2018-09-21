56f4b0b3d8d20a03d4a14103cd45d97d4482682f
Pasi Eronen <pasi.eronen@reaktor.com>
2018-09-21 20:55:17 +0300

    More Docker layer caching testing

--- certbot/build.sh
+++ certbot/build.sh
@@ -3,5 +3,5 @@ set -e
 
 mkdir -p target
 scripts/create-buildversion.sh target/buildversion.txt
-docker build --cache-from koski-luovutuspalvelu-certbot:latest -t koski-luovutuspalvelu-certbot:latest .
+docker build --pull --cache-from koski-luovutuspalvelu-certbot:latest -t koski-luovutuspalvelu-certbot:latest .
 
