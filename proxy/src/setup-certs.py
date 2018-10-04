0394a49e59bceea52341b714870601acc49134da
Pasi Eronen <pasi.eronen@reaktor.com>
2018-10-04 10:21:04 +0300

    Fix indentation

--- proxy/src/setup-certs.py
+++ proxy/src/setup-certs.py
@@ -8,6 +8,6 @@ with open('/etc/ssl/proxy.crt', 'w') as f:
 with open('/etc/ssl/proxy.key', 'w') as f:
     f.write(config['proxyPrivateKey'])
 with open('/etc/ssl/testca.crt', 'w') as f:
-  f.write(config.get('testCaCertificate', ''))
+    f.write(config.get('testCaCertificate', ''))
   
 os.system('cat /etc/ssl/testca.crt /etc/ssl/certs/ca-certificates.crt > /etc/nginx/ca-certificates.crt')
