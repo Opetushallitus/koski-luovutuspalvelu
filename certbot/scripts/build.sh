fbb2f497f1aba6128cf2078ce04edaa3ee4be21c
Henry Heikkinen <henry.heikkinen@reaktor.com>
2019-07-24 15:50:41 +0300

    Don't check base image age during build

--- certbot/scripts/build.sh
+++ certbot/scripts/build.sh
@@ -4,5 +4,3 @@ set -e
 mkdir -p target
 scripts/create-buildversion.sh target/buildversion.txt
 docker build --pull -t koski-luovutuspalvelu-certbot:latest .
-
-../scripts/check-base-image-date.sh Dockerfile 120
