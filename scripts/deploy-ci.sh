1d824c441b7fccb8a9731e60b58dd6a5982f4e30
Juho Majasaari <juho.majasaari@reaktor.com>
2020-10-07 14:30:05 +0300

    Päivitä skripti toimimaan uuden AWS cli:n kanssa

--- scripts/deploy-ci.sh
+++ scripts/deploy-ci.sh
@@ -3,16 +3,18 @@ set -euo pipefail
 
 echo "Pushing images to ECR..."
 
-ECR_REPO="190073735177.dkr.ecr.eu-west-1.amazonaws.com/utility"
+AWS_REGION="eu-west-1"
+ECR_HOST="190073735177.dkr.ecr.${AWS_REGION}.amazonaws.com"
+ECR_REPO="utility"
 DOCKER_TAG_NAME="ci-${TRAVIS_BUILD_NUMBER}"
 
-$(aws ecr get-login --no-include-email)
+aws ecr get-login-password --region ${AWS_REGION} | docker login --username AWS --password-stdin ${ECR_HOST}
 
 for ARTIFACT_NAME in koski-luovutuspalvelu-proxy koski-luovutuspalvelu-certbot; do
-  docker tag ${ARTIFACT_NAME}:latest ${ECR_REPO}/${ARTIFACT_NAME}:latest
-  docker tag ${ARTIFACT_NAME}:latest ${ECR_REPO}/${ARTIFACT_NAME}:${DOCKER_TAG_NAME}
-  docker push ${ECR_REPO}/${ARTIFACT_NAME}:${DOCKER_TAG_NAME}
-  docker push ${ECR_REPO}/${ARTIFACT_NAME}:latest
+  docker tag ${ARTIFACT_NAME}:latest ${ECR_HOST}/${ECR_REPO}/${ARTIFACT_NAME}:latest
+  docker tag ${ARTIFACT_NAME}:latest ${ECR_HOST}/${ECR_REPO}/${ARTIFACT_NAME}:${DOCKER_TAG_NAME}
+  docker push ${ECR_HOST}/${ECR_REPO}/${ARTIFACT_NAME}:${DOCKER_TAG_NAME}
+  docker push ${ECR_HOST}/${ECR_REPO}/${ARTIFACT_NAME}:latest
 done
 
 echo "Images pushed to ECR"
