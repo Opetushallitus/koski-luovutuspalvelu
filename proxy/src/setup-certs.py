bb12db2028b405db3ae3f0ea0f2b05791d255db2
suarela <aleksi.saarela@reaktor.fi>
2021-08-03 14:36:33 +0300

    log allowed ca-certificates for easier debugging

--- proxy/src/setup-certs.py
+++ proxy/src/setup-certs.py
@@ -9,5 +9,7 @@ with open('/etc/ssl/proxy.key', 'w') as f:
     f.write(config['proxyPrivateKey'])
 with open('/etc/ssl/extra-ca-certificates.crt', 'w') as f:
     f.write(''.join([x.strip() + '\n' for x in config.get('extraCaCertificates', [])]))
-  
+
 os.system('cat /etc/ssl/extra-ca-certificates.crt /etc/ssl/certs/ca-certificates.crt > /etc/nginx/ca-certificates.crt')
+os.system('echo accepting following ca-certificates:')
+os.system('cat /etc/nginx/ca-certificates.crt')
