33da857db00c6249239bed49df60b0245d593b52
Juha Rantanen <juha.rantanen@reaktor.fi>
2022-05-25 15:00:04 +0300

    Muutetaan funktiot lokaaleiksi, jolla poistetaan warning mahdollisista sivuvaikutuksista

--- proxy/src/xroad-soap-util.lua
+++ proxy/src/xroad-soap-util.lua
@@ -3,11 +3,11 @@ local tree = require "tree"
 
 local mymodule = {}
 
-function remove_namespace_prefixes(table)
+function mymodule.remove_namespace_prefixes(table)
   local copy = {}
   for k, v in pairs(table) do
     local new_k = string.gsub(k, "(.-):", "")
-    copy[new_k] = type(v) == "table" and remove_namespace_prefixes(v) or v
+    copy[new_k] = type(v) == "table" and mymodule.remove_namespace_prefixes(v) or v
   end
   return copy
 end
@@ -16,7 +16,7 @@ function mymodule.parse_xroad_client_id(req_body)
   local handler = tree:new()
   local parser = xml2lua.parser(handler)
   parser:parse(req_body)
-  local root = remove_namespace_prefixes(handler.root)
+  local root = mymodule.remove_namespace_prefixes(handler.root)
   local client = root.Envelope.Header.client
   print(client)
   local id = client._attr['objectType'] .. ":" .. client.xRoadInstance .. "/" .. client.memberClass .. "/" .. client.memberCode .. "/" .. client.subsystemCode
