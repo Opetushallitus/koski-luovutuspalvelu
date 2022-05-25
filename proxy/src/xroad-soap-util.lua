56d218094ca6ad94838f1c0c9627a81b811dd762
Juha Rantanen <juha.rantanen@reaktor.fi>
2022-05-25 15:25:46 +0300

    Lisätään esimerkki miten voi helposti logittaa lua koodista erilaisia rakenteita

--- proxy/src/xroad-soap-util.lua
+++ proxy/src/xroad-soap-util.lua
@@ -1,5 +1,6 @@
 local xml2lua = require "xml2lua"
 local tree = require "tree"
+local ngx = require "ngx"
 
 local mymodule = {}
 
@@ -12,12 +13,28 @@ function mymodule.remove_namespace_prefixes(table)
   return copy
 end
 
+function mymodule.dump(o)
+    if type(o) == 'table' then
+        local s = '{ '
+        for k,v in pairs(o) do
+            if type(k) ~= 'number' then k = '"'..k..'"' end
+            s = s .. '['..k..'] = ' .. mymodule.dump(v) .. ','
+        end
+        return s .. '} '
+    else
+        return tostring(o)
+    end
+end
+
 function mymodule.parse_xroad_client_id(req_body)
   local handler = tree:new()
   local parser = xml2lua.parser(handler)
+--  Do not enable this on production as hetu will be logged
+  ngx.log(ngx.WARN, mymodule.dump(req_body))
   parser:parse(req_body)
   local root = mymodule.remove_namespace_prefixes(handler.root)
   local client = root.Envelope.Header.client
+  ngx.log(ngx.WARN, mymodule.dump(client))
   local id = client._attr['objectType'] .. ":" .. client.xRoadInstance['1'] .. "/" .. client.memberClass['1'] .. "/" .. client.memberCode['1'] .. "/" .. client.subsystemCode['1']
   return id
 end
