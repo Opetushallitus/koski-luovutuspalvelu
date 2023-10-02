7117bbb6e06929d80e7dbf4b4ddf73c919903361
Henri Ruoho <henri.ruoho@reaktor.com>
2023-10-02 11:10:50 +0300

    Päivitä certbotin Dockerfile ja imagen tägäys

--- scripts/push_images_to_ecr.sh
+++ scripts/push_images_to_ecr.sh
@@ -6,7 +6,7 @@ echo "Pushing images to ECR..."
 AWS_REGION="eu-west-1"
 ECR_HOST="190073735177.dkr.ecr.${AWS_REGION}.amazonaws.com"
 ECR_REPO="utility"
-DOCKER_TAG_NAME="ci-${TRAVIS_BUILD_NUMBER}"
+DOCKER_TAG_NAME="ci-128"
 
 aws ecr get-login-password --region ${AWS_REGION} --profile oph-utility | docker login --username AWS --password-stdin ${ECR_HOST}
 
