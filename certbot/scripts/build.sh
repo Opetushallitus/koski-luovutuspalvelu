4be8c40135886b7f7a5511088755c9bb52a61c53
Henri Ruoho <henri.ruoho@reaktor.com>
2023-10-30 15:57:19 +0200

    Vaadi buildin numero parametrina

--- certbot/scripts/build.sh
+++ certbot/scripts/build.sh
@@ -3,5 +3,5 @@ set -e
 
 mkdir -p target
 scripts/create-buildversion.sh target/buildversion.txt
-../scripts/check-base-image-date.sh Dockerfile 120
+../scripts/check-base-image-date.sh Dockerfile 150
 docker build --pull -t koski-luovutuspalvelu-certbot:latest .
