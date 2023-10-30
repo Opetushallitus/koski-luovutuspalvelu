b4696649fc4a12923c3c678f00181044fd4a5f74
Henri Ruoho <henri.ruoho@reaktor.com>
2023-10-30 15:57:19 +0200

    Päivitä imaget

--- scripts/push_images_to_ecr.sh
+++ scripts/push_images_to_ecr.sh
@@ -6,7 +6,13 @@ echo "Pushing images to ECR..."
 AWS_REGION="eu-west-1"
 ECR_HOST="190073735177.dkr.ecr.${AWS_REGION}.amazonaws.com"
 ECR_REPO="utility"
-DOCKER_TAG_NAME="ci-128"
+
+if [ -z "$DOCKER_TAG_NAME" ]; then
+    echo "Parameter DOCKER_TAG_NAME not provided. See latest version from AWS ECR."
+    exit 1
+fi
+
+# DOCKER_TAG_NAME="ci-128"
 
 aws ecr get-login-password --region ${AWS_REGION} --profile oph-utility | docker login --username AWS --password-stdin ${ECR_HOST}
 
